275a5a3b7dc1604b6a68ae7bbad4c4a26a273d23
will-westover <will.westover@gmail.com>
2026-06-25 11:43:41 -0600

    adding in horizontal one unit movement

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,7 +1,9 @@
 package chess;
 
 import java.util.Collection;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.ArrayList;
 
 /**
  * Represents a single chess piece
@@ -11,7 +13,12 @@ import java.util.List;
  */
 public class ChessPiece {
 
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -30,14 +37,15 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return this.pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
+
     }
 
     /**
@@ -48,6 +56,50 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return List.of();
+        Collection<ChessMove> potentialMoves = new ArrayList<>();
+        ChessPiece piece = board.getPiece(myPosition);
+        switch (piece.getPieceType()){
+            case ROOK -> {
+
+            }
+            case BISHOP -> {
+
+            }
+            case QUEEN -> {
+
+            }
+            case KNIGHT -> {
+
+            }
+            case PAWN -> {
+
+            }
+            case KING -> {
+
+            }
+            case null -> {}
+
+        }
+
+        return potentialMoves;
+    }
+
+    private Collection<ChessMove> horizontalShort(ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> potentialMoves = new ArrayList<>();
+        ChessPiece piece = board.getPiece(myPosition);
+
+        ChessPosition rightPosition =  new ChessPosition(myPosition.getRow(),myPosition.getColumn() + 1);
+        ChessPosition leftPosition =  new ChessPosition(myPosition.getRow(),myPosition.getColumn()-1);
+
+        if (
+                rightPosition.getRow() <= 8 && rightPosition.getRow() > 0
+                && rightPosition.getColumn() <= 8 && rightPosition.getColumn() > 0
+        ){
+            if (board.getPiece(rightPosition) == null) {
+                potentialMoves.add(rightPosition);
+            }
+        }
+        if (leftPosition.)
+        return potentialMoves;
     }
 }
