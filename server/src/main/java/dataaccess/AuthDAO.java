6eb7ca64a6c0a5ab23d28a5fb8bcc8fe2676eb1d
will-westover <will.westover@gmail.com>
2026-07-14 16:10:53 -0600

    DAO for Auth

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -3,5 +3,10 @@ package dataaccess;
 import model.AuthData;
 import java.util.Collection;
 
-public class AuthDAO {
+public interface AuthDAO {
+    void createAuthToken(AuthData authData) throws DataAccessException;
+    AuthData getAuth(String authToken)throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
+    void clear()throws DataAccessException;
+
 }
