1b2b26964e0350fc8d02d09e6d27e0315cfe100e
will-westover <will.westover@gmail.com>
2026-07-14 16:24:39 -0600

    completion of auths

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -4,7 +4,7 @@ import model.AuthData;
 import java.util.Collection;
 
 public interface AuthDAO {
-    void createAuthToken(AuthData authData) throws DataAccessException;
+    void createAuth(AuthData authData) throws DataAccessException;
     AuthData getAuth(String authToken)throws DataAccessException;
     void deleteAuth(String authToken) throws DataAccessException;
     void clear()throws DataAccessException;
--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -3,5 +3,11 @@ package dataaccess;
 import model.GameData;
 import java.util.Collection;
 
-public class GameDAO {
+public interface GameDAO {
+    int createGame(GameData gameData) throws DataAccessException;
+    Collection<GameData> listGames() throws  DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    void updateGame(GameData gameData) throws DataAccessException;
+    void clear() throws DataAccessException;
+
 }
