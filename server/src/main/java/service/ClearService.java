421c40618f07fcf110e8e1549ab7878ed23ccfab
will-westover <will.westover@gmail.com>
2026-07-15 15:38:32 -0600

    ClearService implemenation

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,4 +1,23 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import dataaccess.UserDAO;
+
 public class ClearService {
+    private UserDAO userDAO;
+    private AuthDAO authDAO;
+    private GameDAO gameDAO;
+
+    public ClearService(UserDAO userDAO, AuthDAO authDAO, GameDAO gameDAO){
+        this.userDAO = userDAO;
+        this.authDAO = authDAO;
+        this.gameDAO = gameDAO;
+    }
+    public void clear() throws DataAccessException {
+        userDAO.clear();
+        authDAO.clear();
+        gameDAO.clear();
+    }
 }
