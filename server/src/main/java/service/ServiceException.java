3a09d33c74bd74d0525c18ddbea8d9b02a256704
will-westover <will.westover@gmail.com>
2026-07-15 15:26:58 -0600

    Service files outline

---
+++ server/src/main/java/service/ServiceException.java
@@ -0,0 +1,13 @@
+package service;
+
+public class ServiceException extends Exception {
+    private final int status;
+
+    public ServiceException(int status, String message){
+        super(message);
+        this.status = status;
+    }
+    public int getStatus(){
+        return status;
+    }
+}
