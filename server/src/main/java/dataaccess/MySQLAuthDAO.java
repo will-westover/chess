c7305e7bd69e9b9b116b6d6bf886ba3a6ba2e041
will-westover <will.westover@gmail.com>
2026-07-21 11:16:23 -0600

    MySQLAuth implementation

--- server/src/main/java/dataaccess/MySQLAuthDAO.java
+++ server/src/main/java/dataaccess/MySQLAuthDAO.java
@@ -4,33 +4,33 @@ import model.AuthData;
 
 import java.sql.SQLException;
 
-public class MySQLAuthDAO implements AuthDAO{
+public class MySQLAuthDAO implements AuthDAO {
 
     public void createAuth(AuthData authData) throws DataAccessException {
         var sql = "INSERT INTO auth (authToken, username) VALUES (?,?)";
 
         try (var conn = DatabaseManager.getConnection()) {
-            try(var prepareStatement = conn.prepareStatement(sql)){
+            try (var prepareStatement = conn.prepareStatement(sql)) {
                 prepareStatement.setString(1, authData.authToken());
                 prepareStatement.setString(2, authData.username());
                 prepareStatement.executeUpdate();
             }
 
         } catch (SQLException exception) {
-            throw new DataAccessException("Error: failed to create tables", exception);
+            throw new DataAccessException("Error: failed to create authentication", exception);
         }
     }
 
-    public AuthData getAuth(String authToken) throws DataAccessException{
+    public AuthData getAuth(String authToken) throws DataAccessException {
         var sql = "SELECT authToken, username FROM auth WHERE authToken = ?";
 
         try (var conn = DatabaseManager.getConnection()) {
-            try(var prepareStatement = conn.prepareStatement(sql)){
+            try (var prepareStatement = conn.prepareStatement(sql)) {
                 prepareStatement.setString(1, authToken);
-                try(var result = prepareStatement.executeQuery()){
-                    if (result.next()){
+                try (var result = prepareStatement.executeQuery()) {
+                    if (result.next()) {
                         return new AuthData(result.getString("authToken"),
-                                            result.getString("username"));
+                                result.getString("username"));
                     }
                     return null;
                 }
@@ -41,11 +41,30 @@ public class MySQLAuthDAO implements AuthDAO{
         }
     }
 
-    public void deleteAuth(String authToken) throws DataAccessException{
+    public void deleteAuth(String authToken) throws DataAccessException {
+        var sql = "DELETE FROM auth WHERE authToken = ?";
 
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var prepareStatement = conn.prepareStatement(sql)) {
+                prepareStatement.setString(1, authToken);
+                prepareStatement.executeUpdate();
+            }
+
+        } catch (SQLException exception) {
+            throw new DataAccessException("Error: failed to delete authentication", exception);
+        }
     }
 
-    public void clear() throws DataAccessException{
+    public void clear() throws DataAccessException {
+        var sql = "TRUNCATE TABLE auth";
 
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var prepareStatement = conn.prepareStatement(sql)) {
+                prepareStatement.executeUpdate();
+            }
+
+        } catch (SQLException exception) {
+            throw new DataAccessException("Error: failed to delete authentication", exception);
+        }
     }
 }
