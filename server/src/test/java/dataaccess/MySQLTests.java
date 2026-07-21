cb51d70012cb1b5207b2d2e26c676e358f1c45f7
will-westover <will.westover@gmail.com>
2026-07-20 22:03:05 -0600

    implementation of database manager connection

--- server/src/test/java/dataaccess/MySQLTests.java
+++ server/src/test/java/dataaccess/MySQLTests.java
@@ -1,13 +1,22 @@
 package dataaccess;
-import org.junit.jupiter.api.*;
-import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class MySQLTests {
 
     @Test
-    void verifyConnection() throws Exception{
+    void verifyConnection() throws Exception {
         DatabaseManager.createDatabase();
+        try (var connection = DatabaseManager.getConnection()) {
+            try (var prepareStatement = connection.prepareStatement("SELECT 1 + 1")) {
+                var result = prepareStatement.executeQuery();
+                result.next();
+                assertEquals(2, result.getInt(1));
+            }
+        }
 
     }
 }
