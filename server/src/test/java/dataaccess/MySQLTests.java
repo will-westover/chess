72c78c65246e1ed01c35335e650bb4c5d5935f6a
will-westover <will.westover@gmail.com>
2026-07-20 21:51:46 -0600

    MySQLTests set up

---
+++ server/src/test/java/dataaccess/MySQLTests.java
@@ -0,0 +1,12 @@
+package dataaccess;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class MySQLTests {
+
+    @Test
+    void verifyConnection() throws Exception{
+
+    }
+}
