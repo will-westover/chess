594a225d579d61c2f21ca09377ae8963a7801d54
will-westover <will.westover@gmail.com>
2026-07-21 17:26:14 -0600

    remainder of MySQLTests

--- server/src/test/java/dataaccess/MySQLTests.java
+++ server/src/test/java/dataaccess/MySQLTests.java
@@ -1,6 +1,8 @@
 package dataaccess;
 
+import chess.ChessGame;
 import model.AuthData;
+import model.GameData;
 import model.UserData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,19 +13,29 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MySQLTests {
 
     private MySQLAuthDAO authDAO;
+    private MySQLUserDAO userDAO;
+    private MySQLGameDAO gameDAO;
 
-    private AuthData initializeAuth(){
+    private AuthData makeAuth(){
         return new AuthData("testing", "tester");
     }
-    private UserData initializeUser(){
+    private UserData makeUser(){
         return new UserData("tester", "tester@gmail.com", "password");
+    }
+
+    private GameData makeGame(){
+        return new GameData(123, "player1", "player2","game", new ChessGame() );
     }
 
     @BeforeEach
     void setUp() throws Exception{
         DatabaseManager.createTable();
         authDAO = new MySQLAuthDAO();
+        userDAO = new MySQLUserDAO();
+        gameDAO = new MySQLGameDAO();
         authDAO.clear();
+        userDAO.clear();
+        gameDAO.clear();
     }
 
     @Test
@@ -40,8 +52,7 @@ public class MySQLTests {
     }
     @Test
     void createAuthSuccess() throws Exception{
-        var auth = initializeAuth();
-
+        var auth = makeAuth();
         authDAO.createAuth(auth);
 
         var located = authDAO.getAuth("testing");
@@ -51,100 +62,145 @@ public class MySQLTests {
 
     @Test
     void createAuthDuplicate() throws Exception{
-        var auth = initializeAuth();
-
+        var auth = makeAuth();
         authDAO.createAuth(auth);
+
         assertThrows(DataAccessException.class, () -> authDAO.createAuth(auth));
     }
 
     @Test
     void getAuthSuccess() throws Exception{
+        var auth = makeAuth();
+        authDAO.createAuth(auth);
 
+        assertEquals(auth, authDAO.getAuth("testing"));
     }
 
     @Test
     void getAuthNotFound() throws Exception{
-
+        assertNull(authDAO.getAuth("nope"));
     }
 
     @Test
     void deleteAuthSuccess() throws Exception{
+        var auth = makeAuth();
+        authDAO.createAuth(auth);
+
+        authDAO.deleteAuth("testing");
+        assertNull(authDAO.getAuth("testing"));
 
     }
 
     @Test
     void deleteAuthNonexistent() throws Exception{
-
+        assertDoesNotThrow(() -> authDAO.deleteAuth("nope"));
     }
 
     @Test
-    void clearSuccess() throws Exception{
-
+    void clearAuthSuccess() throws Exception{
+        authDAO.createAuth(makeAuth());
+        authDAO.clear();
+        assertNull(authDAO.getAuth("testing"));
     }
 
     @Test
     void createUserSuccess() throws Exception{
+        var user = makeUser();
+        userDAO.createUser(user);
 
+        assertNull(userDAO.getUser("tester"));
     }
 
     @Test
     void createUserDuplicate() throws Exception{
+        var user = makeUser();
+        userDAO.createUser(user);
 
+        assertThrows(DataAccessException.class, () -> userDAO.createUser(user));
     }
 
     @Test
     void getUserSuccess() throws Exception{
+        var user = makeUser();
+        userDAO.createUser(user);
 
+        assertEquals("tester@gmail.com",userDAO.getUser("tester").email());
     }
 
     @Test
     void getUserNotFound() throws Exception{
-
+        assertNull(userDAO.getUser("nobody"));
     }
 
     @Test
-    void createGameSuccess() throws Exception{
+    void clearUserSuccess() throws Exception {
+        var user = makeUser();
+        userDAO.createUser(user);
 
+        userDAO.clear();
+        assertNull(userDAO.getUser("tester"));
+    }
+
+
+    @Test
+    void createGameSuccess() throws Exception{
+        int id = gameDAO.createGame(makeGame());
+
+        assertTrue(id>0);
     }
 
     @Test
     void createGameNullName() throws Exception{
+        var nullGame = new GameData(0, null,null,null, new ChessGame());
 
+        assertThrows(DataAccessException.class, () ->gameDAO.createGame(nullGame));
     }
 
     @Test
     void getGameSuccess() throws Exception{
+        int id = gameDAO.createGame(makeGame());
 
+        assertEquals("game", gameDAO.getGame(id).gameName());
     }
 
     @Test
     void getGameNotFound() throws Exception{
-
+        assertNull(gameDAO.getGame(9999));
     }
 
     @Test
     void listGameSuccess() throws Exception{
+        int id1 = gameDAO.createGame(makeGame());
+        int id2 = gameDAO.createGame(makeGame());
 
+        assertEquals(2, gameDAO.listGames().size());
     }
 
     @Test
     void listGamesEmpty() throws Exception{
-
-    }
-
-    @Test
-    void listGame() throws Exception{
-
+        assertTrue(gameDAO.listGames().isEmpty());
     }
 
     @Test
     void updateGameSuccess() throws Exception{
+        int id = gameDAO.createGame(makeGame());
 
+        gameDAO.updateGame(new GameData(id, "will", null, "game",
+                            new ChessGame()));
+        assertEquals("will", gameDAO.getGame(id).whiteUsername());
     }
 
     @Test
-    void updateGameNonexistent() throws Exception{
+    void updateGameNonexistent() throws Exception {
+        assertDoesNotThrow(()-> gameDAO.updateGame(new GameData(4378, null,
+                null,"random", new ChessGame())));
+    }
 
+    @Test
+    void clearGameSuccess() throws Exception {
+        int id = gameDAO.createGame(makeGame());
+        gameDAO.clear();
+        assertTrue(gameDAO.listGames().isEmpty());
     }
 
 
