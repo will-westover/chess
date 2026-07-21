228cfe15b65c449fd961fb1b0c4ab009701ece20
will-westover <will.westover@gmail.com>
2026-07-21 14:11:20 -0600

    user mySQL method implementations

--- server/src/main/java/dataaccess/MySQLUserDAO.java
+++ server/src/main/java/dataaccess/MySQLUserDAO.java
@@ -1,17 +1,58 @@
 package dataaccess;
 
+import model.AuthData;
 import model.UserData;
 
-public class MySQLUserDAO {
-    public void createUser(UserData data) throws DataAccessException{
+import java.sql.SQLException;
 
+public class MySQLUserDAO implements UserDAO{
+    public void createUser(UserData data) throws DataAccessException{
+        var sql = "INSERT INTO user (username, email, password) VALUES (?,?,?)";
+
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var prepareStatement = conn.prepareStatement(sql)) {
+                prepareStatement.setString(1, data.username());
+                prepareStatement.setString(2, data.email());
+                prepareStatement.setString(3, data.password());
+                prepareStatement.executeUpdate();
+            }
+
+        } catch (SQLException exception) {
+            throw new DataAccessException("Error: failed to create user", exception);
+        }
     }
 
     public UserData getUser(String username) throws DataAccessException{
+        var sql = "SELECT username, email, password FROM user WHERE username = ?";
 
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var prepareStatement = conn.prepareStatement(sql)) {
+                prepareStatement.setString(1, username);
+                try (var result = prepareStatement.executeQuery()) {
+                    if (result.next()) {
+                        return new UserData(result.getString("username"),
+                                            result.getString("email"),
+                                            result.getString("password"));
+                    }
+                    return null;
+                }
+            }
+
+        } catch (SQLException exception) {
+            throw new DataAccessException("Error: failed to get authentication", exception);
+        }
     }
 
     public void clear() throws DataAccessException{
+        var sql = "TRUNCATE TABLE user";
 
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var prepareStatement = conn.prepareStatement(sql)) {
+                prepareStatement.executeUpdate();
+            }
+
+        } catch (SQLException exception) {
+            throw new DataAccessException("Error: failed to delete user table", exception);
+        }
     }
 }
