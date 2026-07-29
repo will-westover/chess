e9e76aed6a2a513016eb17920225022e069a4345
will-westover <will.westover@gmail.com>
2026-07-28 23:12:40 -0600

    test framework.

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,21 +1,30 @@
 package client;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import server.Server;
+import serverfacade.AuthResponse;
+import serverfacade.ServerFacade;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade facade;
+
+    private AuthResponse registerTestUser() throws Exception{
+        return facade.register("will", "wwest00@byu.edu","password");
+    }
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
+        facade = new ServerFacade(port);
         System.out.println("Started test HTTP server on " + port);
+    }
+    @BeforeEach
+    void clear() throws Exception{
+        facade.clear();
     }
 
     @AfterAll
@@ -23,10 +32,66 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @Test
+    void registerSuccess() throws Exception {
+        var auth = registerTestUser();
+    }
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    void registerDuplicate() throws Exception {
+        var auth = registerTestUser();
     }
+
+    @Test
+    void loginSuccess() throws Exception {
+        var auth = registerTestUser();
+    }
+
+    @Test
+    void loginPasswordError() throws Exception {
+        var auth = registerTestUser();
+    }
+
+    @Test
+    void logoutSuccess() throws Exception {
+        var auth = registerTestUser();
+    }
+
+    @Test
+    void logoutTokenError() throws Exception {
+        var auth = registerTestUser();
+    }
+
+    @Test
+    void createGameSuccess() throws Exception {
+        var auth = registerTestUser();
+    }
+
+    @Test
+    void createGameTokenError() throws Exception {
+        var auth = registerTestUser();
+    }
+
+    @Test
+    void listGameSuccess() throws Exception {
+        var auth = registerTestUser();
+    }
+
+    @Test
+    void listGamesTokenError() throws Exception {
+        var auth = registerTestUser();
+    }
+
+    @Test
+    void joinGameSuccess() throws Exception {
+        var auth = registerTestUser();
+    }
+
+    @Test
+    void joinGameTokenError() throws Exception {
+        var auth = registerTestUser();
+    }
+
+
 
 }
