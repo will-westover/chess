720fc2f56c95a13ddc15d291860ee74a32f16057
will-westover <will.westover@gmail.com>
2026-08-04 16:55:08 -0600

    make move command class implementation

---
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -0,0 +1,16 @@
+package websocket.commands;
+
+import chess.ChessGame;
+import chess.ChessMove;
+
+public class MakeMoveCommand extends UserGameCommand{
+    final private ChessMove move;
+
+    public MakeMoveCommand (String authToken, Integer gameID, ChessMove move){
+        super(CommandType.MAKE_MOVE, authToken, gameID);
+        this.move = move;
+    }
+
+    public ChessMove getMove(){return move;}
+
+}
