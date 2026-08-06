4637701f73e68f5d1ef28f28e9e358b1d53a9bd3
will-westover <will.westover@gmail.com>
2026-08-06 11:25:15 -0600

    Server handler for make move

--- server/src/main/java/server/WebSocketHandler.java
+++ server/src/main/java/server/WebSocketHandler.java
@@ -11,12 +11,17 @@ import websocket.commands.UserGameCommand;
 import websocket.messages.LoadGameMessage;
 import websocket.messages.NotificationMessage;
 import websocket.messages.ErrorMessage;
+import chess.ChessGame;
+import chess.InvalidMoveException;
+import websocket.commands.MakeMoveCommand;
 
 public class WebSocketHandler {
     private final ConnectionManager connections = new ConnectionManager();
     private final AuthDAO authDAO;
     private final GameDAO gameDAO;
     private final Gson gson = new Gson();
+    private final java.util.concurrent.ConcurrentHashMap<Integer, Boolean>
+    gameOver = new java.util.concurrent.ConcurrentHashMap<>();
 
     public WebSocketHandler(AuthDAO authDAO, GameDAO gameDAO) {
         this.authDAO = authDAO;
@@ -28,11 +33,62 @@ public class WebSocketHandler {
             UserGameCommand command = gson.fromJson(message, UserGameCommand.class);
             switch (command.getCommandType()){
                 case CONNECT -> connect(session, command);
+                case MAKE_MOVE -> makeMove(session, gson.fromJson(message, MakeMoveCommand.class));
+                case LEAVE -> leave(session, command);
+                case RESIGN -> resign(session, command);
             }
         }catch (Exception e){
             sendError(session, "Error: " + e.getMessage());
         }
     }
+
+    private void makeMove(Session session, MakeMoveCommand command) throws Exception{
+        AuthData auth = authDAO.getAuth(command.getAuthToken());
+        if(auth == null){sendError(session, "Error: invalid auth token"); return;}
+        GameData gameData = gameDAO.getGame(command.getGameID());
+        if(gameData == null){sendError(session, "Error: invalid game"); return;}
+        if(Boolean.TRUE.equals(gameOver.get(command.getGameID()))){
+            sendError(session, "Error: game is now over"); return;
+        }
+        ChessGame game = gameData.game();
+        String username = auth.username();
+
+        ChessGame.TeamColor color;
+        if(username.equals(gameData.whiteUsername())) color = ChessGame.TeamColor.WHITE;
+        else if(username.equals(gameData.blackUsername())) color = ChessGame.TeamColor.BLACK;
+        else {sendError(session, "Error: observers cannot make moves"); return;}
+
+        if(game.getTeamTurn() != color){sendError(session, "Error: please wait for your turn"); return;}
+        try{
+            game.makeMove(command.getMove());
+        } catch (InvalidMoveException e){
+            sendError(session, "Error: invalid move"); return;
+        }
+
+        gameDAO.updateGame(new GameData(gameData.gameID(), gameData.whiteUsername(),
+                gameData.blackUsername(),gameData.gameName(), game));
+        connections.broadcast(command.getGameID(), null, gson.toJson(new LoadGameMessage(game)));
+        connections.broadcast(command.getGameID(), command.getAuthToken(),
+                gson.toJson(new NotificationMessage(username + " made a move")));
+
+        ChessGame.TeamColor opponent = (color == ChessGame.TeamColor.WHITE)
+                ? ChessGame.TeamColor.BLACK : ChessGame.TeamColor.WHITE;
+        if(game.isInCheckmate(opponent)){
+            connections.broadcast(command.getGameID(), null,
+                    gson.toJson(new NotificationMessage("Checkmate! " + username + " wins")));
+            gameOver.put(command.getGameID(), true);
+        } else if(game.isInStalemate(opponent)){
+            connections.broadcast(command.getGameID(), null,
+                    gson.toJson(new NotificationMessage("Stalemate")));
+            gameOver.put(command.getGameID(), true);
+        } else if (game.isInCheck(opponent)) {
+            connections.broadcast(command.getGameID(), null,
+                    gson.toJson(new NotificationMessage("Check")));
+        }
+    }
+
+    private void leave(Session session, UserGameCommand command) throws Exception{};
+    private void resign(Session session, UserGameCommand command) throws Exception {};
 
     public void connect(Session session, UserGameCommand command) throws Exception{
         AuthData auth = authDAO.getAuth(command.getAuthToken());
