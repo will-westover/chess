04145d3bb3ee9e64fb986de70e5b3e4119a3c6af
will-westover <will.westover@gmail.com>
2026-08-06 10:31:29 -0600

    websocket facade implementation for handling server connection

---
+++ client/src/main/java/websocket/WebSocketFacade.java
@@ -0,0 +1,44 @@
+package websocket;
+
+import com.google.gson.Gson;
+import websocket.commands.UserGameCommand;
+import websocket.messages.*;
+
+import jakarta.websocket.*;
+import java.net.URI;
+
+
+public class WebSocketFacade extends Endpoint {
+    private Session session;
+    private final Gson gson = new Gson();
+    private final ServerMessageObserver observer;
+
+    public WebSocketFacade(int port, ServerMessageObserver observer) throws Exception {
+        this.observer = observer;
+        URI uri = new URI("ws://localhost:" + port + "/ws");
+        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+        this.session = container.connectToServer(this, uri);
+        this.session.addMessageHandler(new MessageHandler.Whole<String>(){
+
+        @Override
+        public void onMessage(String json){
+            ServerMessage base = gson.fromJson(json, ServerMessage.class);
+            ServerMessage full = switch (base.getServerMessageType()){
+                case LOAD_GAME ->  gson.fromJson(json, ServerMessage.class);
+                case NOTIFICATION -> gson.fromJson(json, NotificationMessage.class);
+                case ERROR -> gson.fromJson(json, ErrorMessage.class);
+            };
+            observer.notify(full);
+            }
+        });
+    }
+
+
+
+    public void send(UserGameCommand command) throws Exception{
+        session.getBasicRemote().sendText(gson.toJson(command));
+    }
+
+    @Override
+    public void onOpen(Session session, EndpointConfig config){}
+}
